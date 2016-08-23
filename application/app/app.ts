f1514f347b1c99c96b9ebbb3dc64d7f25cbf768b
rosen-vladimirov <rosen.vladimirov@telerik.com>
2016-08-23 09:14:02 +0300

    Add comment how to use application  in TS

--- application/app/app.ts
+++ application/app/app.ts
@@ -1,4 +1,11 @@
+// The JavaScript way of using module:
+var app = require("application");
+// You will not have autocompletion for app. Writing app.a will not give you any hints.
+
+// The TypeScript way of using module:
 import application = require("application");
 // You must have autocompletion for application.
+// Try typing application.a and check the autocompletion, you must see addCss, android, etc.
+// If the project does not have required .d.ts files for tns-core-modules, the import statement will fail during transpilation.
 
 application.start({ moduleName: "main-page" });
